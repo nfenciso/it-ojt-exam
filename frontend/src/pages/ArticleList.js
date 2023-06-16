e751c7ac0a286ee4005a77288278e3882f33f71f
nfenciso <nfenciso@up.edu.ph>
2023-06-17 03:24:22 +0800

    style: design the fetched data, and add the top portion required

--- frontend/src/pages/ArticleList.js
+++ frontend/src/pages/ArticleList.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import data from "./assets/MOCK_DATA.json";
-import dragImage from "./assets/drag_indicator_square.png";
+import dragImage from "./assets/drag_indicator_square_cropped.png";
+import profileImage from "./assets/account_circle_green.png";
+import dateImage from "./assets/date_range_green.png";
+import viewImage from "./assets/visibility_blue.png";
 
 export default class ArticleList extends Component {
 
@@ -10,41 +13,68 @@ export default class ArticleList extends Component {
   }
 
   render() {
-  
     return(
-        <table>
+        <>
+            <h2>News Articles</h2>
+
+            <div id="topleft">
+            <input type="checkbox" style={{verticalAlign: "middle"}} />
+            <button className="publishButton">Publish</button>
+            <button className="deleteButton">Delete</button>
+            </div>
+            
+            <input type="text" id="searchBar" placeholder="Search ..."></input>
+            
+            <br/>
+            <br/>
             {
                 data.map((article) => {
-                    let content;
                     if (article.content.length > 80) {
-                        content = article.content.substring(0,80).concat("...")
+                        return(
+                            <div className="articleDiv" key={article.id+"-div"}>
+                            <table>
+                                <tbody>
+                                <tr className="firstSubRow">
+                                    <td className="firstCol"><img className="dragImage" src={dragImage} alt="drag" /></td>
+                                    <td className="secondCol"><input type="checkbox" /></td>
+                                    <td className="thirdCol"><span className="articleTitle">{article.title}</span></td>
+                                    
+                                </tr>
+                                <tr className="secondSubRow">
+                                    <td className="firstCol"></td>
+                                    <td className="secondCol"></td>
+                                    <td className="thirdCol">
+                                        <img className="profileImage" src={profileImage} alt="account" />
+                                        <div className="author">{article.author}</div>
+                                        <img className="dateImage" src={dateImage} alt="calendar" />
+                                        <div className="date">{article.date}</div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="firstCol"></td>
+                                    <td className="secondCol"></td>
+                                    <td className="thirdCol">
+                                        <div className="content">{article.content.substring(0,80).concat("...")}</div>
+                                        <img className="viewImage" src={viewImage} alt="view" />
+                                        <span className="readfull">Read Full</span>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                            <div className="hashtags">
+                                <button className="hashtagButton">#Sports</button>
+                                <button className="hashtagButton">#Worldwide</button>
+                                <button className="hashtagButton">#Local</button>
+                            </div>
+                            </div>
+                        )
                     } else {
-                        content = article.content
+                        return(<></>)
                     }
 
-                    return(
-                        <div>
-                            <tr>
-                                <td>
-                                    <img className="dragImage" src={dragImage} /><input type="checkbox" /></td>
-                                <td>{article.title}</td>
-                                <td>| #Sports | #Worldwide | #Local |</td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td>{article.author} {article.date}</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td>{content}</td>
-                                <td></td>
-                            </tr>
-                        </div>
-                    )
                 })
             }
-        </table>
+        </>
     )
   }
 }
